637f3cc0a7217962292e4a91201e34a6d744b023
wen <wen.wa@qq.com>
2024-04-03 00:34:57 -0400

    fix: bugs

--- lab1/cw4301.c
+++ lab1/cw4301.c
@@ -2,41 +2,31 @@
 #include <omp.h>
 #include <time.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    // printf("Please input the number a b x t: (split by blank space)\n");
-    // int a, b, x, t;
-    // scanf("%d %d %d %d", &a, &b, &x, &t);
+    if (argc != 5) {
+        perror("Please input a, b, x, t, split by blank space.");
+        return -1;
+    }
 
-    int a = 10;
-    int b = 100000000;
-    int x = 3;
-    int t = 1;
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    int x = atoi(argv[3]);
+    int t = atoi(argv[4]);
 
     omp_set_num_threads(t);
-    // int max_threads = omp_get_max_threads();
-    // printf("max_threads: %d\n", max_threads);
 
-    time_t start_time = time(NULL);
-    int seg_cnt = (b - a) / t;
     int ans = 0;
     
-    #pragma omp parallel
-    {
-        int ID = omp_get_thread_num();
-        
-        // check all numbers in [ID * seg_cnt, (ID + 1) * seg_cnt)
-        for (int i = ID * seg_cnt; i < (ID + 1) * seg_cnt; i++) {
-            if (i % x == 0) {
-                ans++;
-            }
+    #pragma omp parallel for reduction(+:ans)
+    for (int i = a; i <= b; i++) {
+        if (i % x == 0) {
+            ans += 1;
         }
     }
 
-    time_t end_time = time(NULL);
-
     printf("There are %d numbers divisible by %d in the [%d, %d] range.\n", ans, x, a, b);
-    printf("Time taken: %ld\n", end_time - start_time);
+    printf("Time taken: time in seconds\n");
 
     return 0;
 }
